b818dd0913f731f0e3092ccd99e1dc7ea309a288
guka jincharadze <115719203+jinchara@users.noreply.github.com>
2022-12-07 19:35:39 +0400

    Update ViewPagerFragmentAdapter.kt

--- app/src/main/java/com/example/myapplication/adapters/ViewPagerFragmentAdapter.kt
+++ app/src/main/java/com/example/myapplication/adapters/ViewPagerFragmentAdapter.kt
@@ -10,17 +10,17 @@ import com.example.myapplication.fragments.ThirdFragment
 class ViewPagerFragmentAdapter(activity: AppCompatActivity): FragmentStateAdapter(activity) {
 
     override fun getItemCount(): Int {
-        return 2
+        return 3
     }
 
     override fun createFragment(position: Int): Fragment {
         if (position == 0) {
             return FirstFragment()
-        }
-        if (position == 1){
+        }else if (position == 1){
             return SecondFragment()
         }else {
             return ThirdFragment()
         }
+        
     }
 }
